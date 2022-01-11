a4f782ce2d77639e17e7b0d69b42b40cce6372b7
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-01-11 15:20:53 +0100

    add empty main Window

---
+++ src/frontend.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+class Window(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title("Jakobsstab")
+        self.geometry("300x100")
+        
+
+        
+    
+
+
+
+window=Window()
+window.mainloop()
