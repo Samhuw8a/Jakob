7ae17fbc2492652c4c9fd57c7ac87e4b59aec1eb
Samhuw8a <77882404+Samhuw8a@users.noreply.github.com>
2022-01-11 19:43:19 +0100

    Updated backend

--- src/backend.py
+++ src/backend.py
@@ -1,49 +1,66 @@
 import yaml
+import math
 
 class Settings():
-    #TODO:Backup comparison
     def __init__(self,path:str,backup_path:str)->None:
         self.path:str = path
         self.backup_path:str = backup_path
 
-    def is_valid(self,setts:dict)->dict:
+    def ret_valid(self,setts:dict)->dict:
         cur:dict = self.backup_settings
-        if len(cur)!=len(setts):
+        if cur.keys()!=setts.keys():
             return cur
-
-        for i,j in zip(cur.keys(),setts.keys()):
-            if i!=j:
-                return cur
         return setts
 
-    @property
-    def backup_settings(self)->dict:
-        with open(self.backup_path,"r") as f:
+    def read_conf(self,path:str)->dict:
+        with open(path,"r") as f:
             config = yaml.load(f,Loader=yaml.FullLoader)
         return config
 
     @property
+    def backup_settings(self)->dict:
+        return self.read_conf(self.backup_path)
+
+    @property
     def settings(self)->dict:
-        with open(self.path,"r") as f:
-            config = yaml.load(f,Loader=yaml.FullLoader)
-        return self.is_valid(config)
+        res = self.read_conf(self.path)
+        return self.ret_valid(res)
 
     def write(self,sett:dict)->None:
-        if self.is_valid(sett) == sett:
+        if self.ret_valid(sett) == sett:
             with open(self.backup_path,"w") as f:
                yaml.dump(sett,f)
 
         with open(self.path,"w") as f:
            yaml.dump(sett,f)
-            
 
 class Handler():
-    def __init__(self,settings_path:str,backup_path:str):
+    def __init__(self,settings_path:str,backup_path:str)->None:
         self.settings_path = settings_path
-        self.conf= Settings(settings_path,backup_path)
+        self.conf = Settings(settings_path,backup_path)
+
+    def solve_Apian(self,height:float,distance:float,a:float,b:float)->float:
+        return height +distance*math.tan(
+            2 * math.atan(b/(2*a))
+            + math.atan(distance/(height)
+            -90))
+        #  return ((distance*b)/a)
+
+    def solve_Strahlen(self,height:float,distance:float,a:float,b:float)->float:
+        return height+(
+            (distance*b)/
+            (2*a)
+        )
 
     def solve(self,height:float,distance:float)->float:
-        return 0.0
+        c = self.conf.settings
+        a = c["stab_laenge"]
+        b = c["stab_hoehe"]
+
+        if c["mess_methode"] == "Apian":
+            return self.solve_Apian(height,distance,a,b)
+        else:
+            return self.solve_Strahlen(height,distance,a,b)
 
     def get_conf(self)->dict:
         return self.conf.settings
@@ -52,10 +69,9 @@ class Handler():
         self.conf.write(sett)
 
 def main()->None:
-    h = Handler("Config.yaml")
+    h = Handler("Config.yaml","Backup.yaml")
     c  =h.get_conf()
-    print(c)
-    c["test"]=False
+    print(h.solve(1.0,10))
     h.set_conf(c)
     
 if __name__=='__main__':
