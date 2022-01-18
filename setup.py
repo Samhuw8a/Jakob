ea2136729a39beba82bf4a375208dbd74c2e6f00
Samhuw8a <Samuel.huwiler@gmx.ch>
2022-01-18 15:34:01 +0100

    Build

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {"packages": ["pyyaml"], "excludes": ["tkinter"]}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name = "Jakobsstab",
+    version = "0.1",
+    description = "Jakobsstab Projekt",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("Jakobssatb.py", base=base)]
+)
